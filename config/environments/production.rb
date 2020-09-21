d8908d7d099a2ede3ff0de9c66018ebe099e10e8
Luiz Salvalagio <luizfernandosalvalagio@gmail.com>
2020-09-21 19:36:45 -0300

    found on the internet sugestion to fix problems with images on heroku https://medium.com/@madsen.tyler/why-your-rails-images-are-broken-on-heroku-and-how-to-fix-them-be48e386b067

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -28,6 +28,11 @@ Rails.application.configure do
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
 
+
+  APPLICATIONNAME::Application.configure do
+     config.assets.compile = true
+  end
+
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.action_controller.asset_host = 'http://assets.example.com'
 
